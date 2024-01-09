3ae6b6e362dd30a493e7fb0742f41421d176bb78
NarendraVarma87 <narendravarma97047@gmail.com>
2024-01-09 22:43:39 +0530

    day 3

---
+++ day3.js
@@ -0,0 +1,33 @@
+/* Loops
+while , for , do while
+*/
+//while loop
+let num = 1;
+while(num <= 10){
+    console.log("Hi" +" "+num);
+    num++;
+}
+
+console.log("+*********************************+");
+//do-while loop
+function doWhile(){
+    let value = 10;
+    do{
+        console.log("Hi "+ value);
+        value--;
+    }while(value > 0);
+}
+doWhile();
+
+//for loop
+function forLoop(){
+    let number = 30;
+    for(let i = 1 ; i < number ; i++){
+        if(number % i == 0){
+            console.log("Even Number "+ i);
+        }else{
+            console.log("Odd Number "+ i);
+        }
+    }
+}
+forLoop();
